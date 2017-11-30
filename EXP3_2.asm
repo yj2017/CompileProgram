179cda3658fdf8b49543ec58a9859d7c1f3d05cf
yangjin <1424783824@qqcom>
2017-11-30 20:59:33 +0800

    od

--- EXP3_2.asm
+++ EXP3_2.asm
@@ -1,11 +1,11 @@
 DATAS SEGMENT 
-	MSG1   DB  13, 10, 'Number: ', 13, 10, 13, 10, '$'  
-	MSG2   DB  13, 10, 'Number: ', 13, 10, 13, 10, '$' 
-	MSG3   DB  13, 10, 'Number: ', 13, 10, 13, 10, '$' 
+	MSG1   DB 13,10, 'number: ' , '$'
+	MSG2   DB  13,10, 'word: ', '$'
+	MSG3   DB 13,10,'others: ' , '$'
     string DB 200 DUP(?)
-    numC DW 0;数字 计数器
-    wordC DW 0;字母计数器
-    OthersC DW 0;其他字符计数器
+    numC DB 30H;数字 计数器
+    wordC DB 30H;字母计数器
+    OthersC DB 30H;其他字符计数器
 DATAS ENDS
 
 STACKS SEGMENT
@@ -17,67 +17,66 @@ CODES SEGMENT
 START:
     MOV AX,DATAS
     MOV DS,AX
-    LEA DI, string
-
-    MAIN PROC FAR;MAIN主子程序
-    PUSH DI;保存参数值
- INPUT:
-    MOV AH,01
+    
+  	MOV DX,offset string
+    MOV AH,0AH
     INT 21H
-    MOV [DI],AL;输入的内容存进string
-    INC DI
-    CMP BX,0DH
-    JNZ INPUT;继续输入
-    ;输入为回车，计算
+    MOV AH,09H
+	SUB CH,CH
+    MOV CL,string+1;字符串个数
+    LEA DI,string+2;输入的内容存进string
     CALL NUMBER
-    CALL WORD
+    CALL WORDS
     CALL OTHERS
-    MOV DL,MSG1;提示数字计数：
-    MOV AH,02
+    
+    MOV DX,OFFSET MSG1;提示数字计数：
+    MOV AH,09H
     INT 21H
     MOV DL,NUMC
-    MOV AH,02
+    MOV AH,02H
     INT 21H
-    MOV DL,MSG2;提示字母计数：
-    MOV AH,02
+    MOV DX,OFFSET MSG2;提示字母计数：
+    MOV AH,09H
     INT 21H
     MOV DL,WORDC
-    MOV AH,02
+    MOV AH,02H
     INT 21H
-    MOV DL,MSG3;提示其他字符计数：
-    MOV AH,02
+    MOV DX,OFFSET MSG3;提示其他字符计数：
+    MOV AH,09H
     INT 21H
     MOV DL,OTHERSC
-    MOV AH,02
+    MOV AH,02H
     INT 21H
-    POP DI
-    RET
-    MAIN ENDP
+    MOV AH,4CH
+    INT 21H
+    
+   
 ;数字计数  
     NUMBER PROC NEAR
     PUSH DI
 COMP1: 
 	MOV DX,[DI];暂存DX寄存器
-	INC DI;
-    CMP DX,ODH
+	INC DI
+    CMP DX,0DH
     JZ EXIT1
     CMP DX,30H
-    JB COMP;小于0,判断下一个字符
+    JB COMP1;小于0,判断下一个字符
     CMP DX,39H
-    JA COMP  
+    JA COMP1  
 	INC NUMC;计数器加一
-	JMP COMP
-EXIT1：	
+	JMP COMP1
+EXIT1:	
 	POP DI
     RET 
+    
     NUMBER ENDP
 ;字母计数      
-    WORD PROC NEAR  
+    WORDS PROC NEAR  
     PUSH DI
 COMP2: 
 	MOV DX,[DI]
-	INC,DI
-    CMP DX,ODH
+	INC DI
+    CMP DX,13
     JZ EXIT2
     CMP DX,41h
     JB COMP2
@@ -93,13 +92,14 @@ COUNT:
 EXIT2:
 	POP DI
     RET 
-    WORD ENDP
+    
+    WORDS ENDP
 ;其他字符计数    
     OTHERS PROC NEAR
     PUSH DI
 COMP3: 
 	MOV DX,[DI]
-    CMP DX,ODH
+    CMP DX,0DH
     JZ EXIT3
     CMP DX,30H
     JB COUNTothers
@@ -117,15 +117,14 @@ COUNTothers:
 	INC OTHERSC
 	JMP COMP3
 	
-EXIT3：	
+EXIT3:
     RET 
     POP DI
     OTHERS ENDP
-;结束
-    MOV AH,4CH
-    INT 21H
+
 CODES ENDS
     END START
+
 
 
 
